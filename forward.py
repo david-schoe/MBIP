84c22fab4cf01e54ed65d5e2e1493e6010ce1dfb
david-schoe <dschoenb@uccs.edu>
2024-08-18 10:24:44 -0600

    second commit

--- forward.py
+++ forward.py
@@ -1,11 +1,14 @@
-qimport numpy as np
+import sys
+import numpy as np
 import scipy.special as sp
-import matplotlib.pyplot as plt
+
+from PIL import Image, ImageDraw
+from matplotlib.figure import Figure
 
 R_0 = 0;
 m = 0;
 theta = np.zeros(100);
-phi = np.linspace(np.pi,0,100);
+phi = np.linspace(0,np.pi,100);
 
 A = lambda n : 1/n/n;
 Y = lambda n,m,theta,phi : sp.sph_harm(m,n,theta,phi);
@@ -16,13 +19,19 @@ R = R_0;
 
 for n in range(2,3):
 	R = R + R_n(n,theta,phi);
-print (R);
-x = R*np.sin(phi);
-y = R*np.cos(phi);
-nx = -x;
 
-plt.plot(x,y,nx,y);
-plt.axis('equal');
-plt.show();
+px = R*np.sin(phi);
+py = R*np.cos(phi);
+nx = np.flip(-px);
+ny = np.flip(py);
+x = np.concatenate((px,nx));
+y = np.concatenate((py,ny));
+
+fig = Figure();
+ax = fig.add_subplot();
+ax.set_axis_off();
+ax.set_aspect('equal');
+ax.plot(x,y);
+fig.savefig("test.png");
 
 
